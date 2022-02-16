9e6daab6bceda117bced6f3d3c7e975bf7f98e70
RPMMartins <83402847+RPMMartins@users.noreply.github.com>
2022-02-16 23:26:41 +0100

    Update percent.py
    Nova versão do programa 22-2-16

--- percent.py
+++ percent.py
@@ -1,6 +1,15 @@
 
+#pergunta ao utilizador se introduz comida ou razão
+
+tipo=input('''
+Comida: c  
+Ração: r
+Comida ou Ração: ''')
+
+
+
 #pergunta ao utilizador a quantidade de ração
-quat = input("Qual é a quantidade de ração: ")
+quat = input("Qual é a quantidade: ")
 
 def isfloat(num):
     try:
@@ -11,9 +20,21 @@ def isfloat(num):
 
 while not isfloat(quat):
     quat = input("Quantidade invalida\nQual é a quantidade de ração: ")
+
+
 quat = float(quat)
 
-PR = quat / 300
-PC = 1 - PR
-print(f"Percentagem de comida: {round(100* PC,2)}%")
-print(f"Quantidade de comida: {round(750 * PC)}")
+
+
+if tipo == 'r':
+    PR = quat / 350
+    PC = 1 - PR
+    print()
+    print(f"Percentagem de comida: {round(100* PC,2)}%")
+    print(f"Quantidade de comida: {round(750 * PC)}")
+else:
+    PR = quat / 750
+    PC = 1 - PR
+    print()
+    print(f"Percentagem de comida : {round(100* PR,2)}%")
+    print(f"Quantidade de ração: {round(350 * PC)}")
