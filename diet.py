ae2df28857fa9633e71e6f79fee55671d946d568
RPMMartins <83402847+RPMMartins@users.noreply.github.com>
2022-02-25 20:03:43 +0100

    Acrescentar a opção de introduzir total de comida

--- diet.py
+++ diet.py
@@ -1,6 +1,8 @@
 #lista de ingredients na dieta dos caes
 ings =['A', 'C', 'H', 'V', 'F']
 
+nomes=['A', 'C', 'H', 'V', 'F', "T"]
+
 perc = { 
     "C" : 0.25,
     "H" : 0.2,
@@ -13,14 +15,21 @@ nomes ={
     "H" : "Hidratos de Carbono",
     "V" : "Vegetais",
     "A" : "Arroz",
-    "F" : "Figado"
+    "F" : "Figado",
+    "T" : "Total"
 }
 
 
 #pede ao utilizador o ingredient e a quantidade
-ing = input("Qual é o ingrediente: ").upper()
+ing = input('''    C : Carne
+    H : Hidratos de Carbono
+    V : Vegetais
+    A : Arroz
+    F : Figado
+    T : Total de Comida
+Qual é o ingrediente: ''').upper()
 
-while ing not in ings:
+while ing not in nomes:
     print('Ingredient invalido')
     ing = input("Qual é o ingrediente: ").upper()
 
@@ -36,15 +45,20 @@ def isfloat(num):
 while not isfloat(quat):
     quat = input("Quantidade invalida\nQual é a quantidade: ")
 quat = float(quat)
-### os dados estão validos para computação ###    
 
-#calcular o total da reifeição
-total = quat / perc[ing]
+
+### os dados estão validos para computação ###    
+if ing != "T":
+    #calcular o total da refeição
+    total = quat / perc[ing]
+else:
+    total=quat
 
 #imprimir as quantidades necessariaas de cada ingredient
 for j in ings:
     print(f"{nomes[j]}: {total * perc[j]}")
 print(f'Total: {total}')
+    
 
 
 
